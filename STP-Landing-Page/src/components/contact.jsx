6febe7d7d75925f25fb20b72ffcb7ffbebf5d32f
Wsprung <whitneykin@gmail.com>
2023-04-06 15:53:15 -0400

    Adds order form, changes instagram and email icons, and adds logo to header

--- STP-Landing-Page/src/components/contact.jsx
+++ STP-Landing-Page/src/components/contact.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import emailjs from "emailjs-com";
 import React from "react";
 
 const initialState = {
-  name: "",
-  email: "",
-  message: "",
+  name: '',
+  email: '',
+  chickenNoodle: false,
+  tortilla: false,
+  butternutSquash: false,
+  customization: ' ',
 };
 export const Contact = (props) => {
-  const [{ name, email, message }, setState] = useState(initialState);
+  const [{ name, email, chickenNoodle, tortilla, butternutSquash, customization }, setState] = useState(initialState);
+  const [chickenNoodleChecked, setCN ] = useState(false);
+  const [tortillaChecked, setT ] = useState(false);
+  const [butternutSquashChecked, setBNS ] =useState (false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -16,21 +22,68 @@ export const Contact = (props) => {
   };
   const clearState = () => setState({ ...initialState });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(name, email, message);
-    emailjs
-      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
-      .then(
-        (result) => {
-          console.log(result.text);
-          clearState();
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+  function handleCheckboxChange(event, value) {
+    event.preventDefault();
+   
+    if(value == 1) {
+      setCN(event.target.checked);
+      setState(prevState => ({
+        ...prevState,
+        chickenNoodle: true
+      }));
+    }
+    else if(value == 2) {
+      setT(event.target.checked);
+      setState(prevState => ({
+          ...prevState,
+          tortilla: true
+        }));
+    }
+    else if(value == 3) {
+      setBNS(event.target.checked);
+      setState(prevState => ({
+        ...prevState,
+        butternutSquash: true
+      }));
+      
+      //setIsChecked(event.target.checked); // Setting checkbox to checked for UI, also sets isChecked to true (need to keep to change box to checked)
+  }
+  }
+
+  // function that will handle the form submission
+  // uses the fetch API to send the data to backend API using the POST method
+  // using sheet.best API
+  // when the form is submitted, the handleSubmit function will be called and the data will be sent to the backend API
+  async function handleSubmit(event) {
+    event.preventDefault();
+  
+    // using the fetch function to make POST request to sheet.best endpoint
+    const response = await fetch('https://sheet.best/api/sheets/2ad570f7-1120-4a4f-bb3c-eb2146ac3827', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name, email, chickenNoodle, tortilla, butternutSquash, customization }), // passing in formData object as request body
+    });
+    clearState();
+
+  }
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(name, email, message);
+  //   emailjs
+  //     .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
+  //     .then(
+  //       (result) => {
+  //         console.log(result.text);
+  //         clearState();
+  //       },
+  //       (error) => {
+  //         console.log(error.text);
+  //       }
+  //     );
+  // };
   return (
     <div>
       <div id="contact">
@@ -38,10 +91,9 @@ export const Contact = (props) => {
           <div className="col-md-8">
             <div className="row">
               <div className="section-title">
-                <h2>Get In Touch</h2>
+                <h2>ORDER FORM</h2>
                 <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
+                  COMING SOON: Use this form below to place an order of our delicious soup just like your mom makes it.
                 </p>
               </div>
               <form name="sentMessage" validate onSubmit={handleSubmit}>
@@ -76,20 +128,40 @@ export const Contact = (props) => {
                   </div>
                 </div>
                 <div className="form-group">
-                  <textarea
-                    name="message"
-                    id="message"
-                    className="form-control"
-                    rows="4"
-                    placeholder="Message"
-                    required
-                    onChange={handleChange}
-                  ></textarea>
-                  <p className="help-block text-danger"></p>
+                  <div className = "checkbox-soup">
+                    <label className = "check-the-soups">Check off the soup(s) you are ordering</label>
+                    <label>
+                      <input type="checkbox"
+                       checked={chickenNoodleChecked} onChange={(e) => handleCheckboxChange(e, 1)} />
+                      Chicken Noodle Soup
+                    </label>
+                    <label>
+                      <input type="checkbox" checked={tortillaChecked} onChange={(e) => handleCheckboxChange(e, 2)} />
+                      Tortilla Soup
+                    </label>
+                    <label>
+                      <input type="checkbox" checked={butternutSquashChecked} onChange={(e) => handleCheckboxChange(e, 3)} />
+                      Vegan Butternut Squash
+                    </label>
+                    <p className="help-block text-danger"></p>
+                      <div className="form-group">
+                        <textarea
+                          name="customization"
+                          id="customization"
+                          className="form-control"
+                          rows="1"
+                          placeholder="Customization"
+                          required
+                          onChange={handleChange}
+                        ></textarea>
+                        <p className="help-block text-danger"></p>
+                      </div>
+                  </div>
                 </div>
                 <div id="success"></div>
+                {/* CHANGE H1 TO BUTTON ONCE FORM ALLOWS ORDERS TO SUBMIT */}
                 <button type="submit" className="btn btn-custom btn-lg">
-                  Send Message
+                  Coming Soon
                 </button>
               </form>
             </div>
@@ -126,34 +198,19 @@ export const Contact = (props) => {
               <div className="social">
                 <ul>
                   <li>
-                    <a href={props.data ? props.data.facebook : "/"}>
-                      <i className="fa fa-facebook"></i>
+                    <a href={props.data ? props.data.instagram : "/"}>
+                      <i className="fa fa-instagram"></i>
                     </a>
                   </li>
                   <li>
-                    <a href={props.data ? props.data.twitter : "/"}>
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
+                    <a href={props.data ? props.data.email : "/"}>
+                      <i className="fa fa-envelope"></i>
                     </a>
                   </li>
                 </ul>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div id="footer">
-        <div className="container text-center">
-          <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
-            </a>
-          </p>
         </div>
       </div>
     </div>
--- STP-Landing-Page/src/components/header.jsx
+++ STP-Landing-Page/src/components/header.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 export const Header = (props) => {
   return (
+    
     <header id="header">
       <div className="intro">
         <div className="overlay">
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
+              <img src = "../public/img/header-logo.png" alt = "Logo" className = "header-STP-logo"/>
                 <h1>
-                  {props.data ? props.data.title : "Loading"}
                   <span></span>
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
