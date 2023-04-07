1088fb4ca96eada3d353c7bfa7da559add8234d9
lmo66 <124401057+lmo66@users.noreply.github.com>
2023-04-07 13:05:56 -0400

    Add files via upload

--- STP-Landing-Page/src/components/Team.jsx
+++ STP-Landing-Page/src/components/Team.jsx
@@ -5,22 +5,20 @@ export const Team = (props) => {
     <div id="team" className="text-center">
       <div className="container">
         <div className="col-md-8 col-md-offset-2 section-title">
-          <h2>Meet the Team</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
-          </p>
+          <h2>Meet the Founders</h2>
         </div>
+        <br/><br/>
         <div id="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                <div key={`${d.name}-${i}`} className="col-md-6 col-sm-6 team">
                   <div className="thumbnail">
                     {" "}
                     <img src={d.img} alt="..." className="team-img" />
                     <div className="caption">
                       <h4>{d.name}</h4>
-                      <p>{d.job}</p>
+                      <h5>{d.job}</h5>
+                      <p>{d.bio}</p>
                     </div>
                   </div>
                 </div>
